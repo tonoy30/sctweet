e3e1b8c54a4c669c6888734d26478531fe078d55
_tonoy_ <tonoy@monetizenow.io>
2023-03-05 04:07:18 +0600

    auth controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,15 +1,23 @@
-class RegistrationController < ApplicationController
+class AuthController < ApplicationController
   def signup
     @user = User.new
   end
+
   def createuser
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path, notice: "User created successfully"
     else
-      render :signup
+      render :signup, status: 400
     end
   end
+
+  def logout
+    session[:user_id] = nil
+    redirect_to root_path, notice: "User logout successfully"
+  end
+
   private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   get "about", to: "about#index"
 
-  get "signup", to: "registration#signup"
-  post "signup", to: "registration#createuser"
+  get "signup", to: "auth#signup"
+  post "signup", to: "auth#createuser"
+  delete "logout", to: "auth#logout"
 
   root to: "main#index"
 end
